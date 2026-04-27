82c6efaba533d14eb5d98017a4fb5e8005565aa0
MARCOS v <priottomarcos7@gmail.com>
2026-04-27 14:29:01 -0300

    allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require ("allure-cypress/reporter");
+
 
 module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
     baseUrl:"http://localhost:3000/",
     projectId: "5wjx9f",
